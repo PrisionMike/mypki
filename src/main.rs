b51a12661c10067980a7afb8dfef3d7430ca1feb
PrisionMike <su.sh2396@gmail.com>
2021-05-26 18:54:09 +0530

    futile attempts at generating rand int

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use ramp::int::Int;
+use ramp::int::RandomInt;
+// use ramp::RandomInt;
+use rand::rngs::OsRng;
+use rand;
+
 fn main() {
-    println!("Hello, world!");
+    // let gma = Int::from(587454);
+    let mut rng = rand::thread_rng();
+    let gma:Int = rng.gen_uint(256);
+    println!("{}",gma);
 }
