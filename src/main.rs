061a95ff9f234c1fc27292b88c78d8943a295161
PrisionMike <su.sh2396@gmail.com>
2021-05-27 16:36:25 +0530

    removed useless comment

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-// use ramp::int::Int;
-// use ramp::int::RandomInt;
 use ramp::Int;
 use ramp::RandomInt;
-// use rand::rngs::OsRng;
-// use rand::thread_rng;
-// use rand::rngs::OsRng;
 use rand::prelude::*;
 
-// extern crate rand;
-// extern crate ramp;
-
 fn main() {
-    // let gma = Int::from(587454);
-    // let mut rng = rand::thread_rng();
+
     let mut rng = thread_rng();
-    // let mut rng = OsRng;
-    // let gma:Int = rng.gen_uint(256);
     let gma: Int = rng.gen_uint(2048);
+
     println!("{}",gma);
 }
