576a9196c0fb8f053ee95047572356695f0b816a
PrisionMike <su.sh2396@gmail.com>
2021-05-27 13:31:59 +0530

    rand int gen suc. after changing rand ver

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 // use ramp::int::Int;
 // use ramp::int::RandomInt;
+use ramp::Int;
 use ramp::RandomInt;
 // use rand::rngs::OsRng;
-use rand;
+// use rand::thread_rng;
+// use rand::rngs::OsRng;
+use rand::prelude::*;
 
-extern crate rand;
-extern crate ramp;
+// extern crate rand;
+// extern crate ramp;
 
 fn main() {
     // let gma = Int::from(587454);
-    let mut rng = rand::thread_rng();
-    let gma:Int = rng.gen_uint(256);
+    // let mut rng = rand::thread_rng();
+    let mut rng = thread_rng();
+    // let mut rng = OsRng;
+    // let gma:Int = rng.gen_uint(256);
+    let gma: Int = rng.gen_uint(2048);
     println!("{}",gma);
 }
