51c109ba7640f99da0ba17ade0d4fb78e3e36f06
PrisionMike <su.sh2396@gmail.com>
2021-05-31 22:40:16 +0530

    timing comments. improved organisation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ramp::Int;
 use ramp::RandomInt;
 use rand::prelude::*;
 use mypki::SMALL_PRIMES;
+// use std::time::{Duration, Instant};
 
 // static mut rng: ThreadRng = thread_rng();
 
@@ -37,21 +38,18 @@ fn main() {
 
 fn prime_eh(n: &Int) -> bool {
     
-    // Fake execution. Just checking the 69th bit.
-    // if n.bit(69) {
-    //     true
-    // } else { false }
+    let mr_rounds : u8 = 10;
 
-    for p in SMALL_PRIMES.iter() {
-
-        // println!("testing with {}",p);
-        if n % Int::from(*p) == 0 { return false }
-    }
+    if !primitive_primality_test(n) {
+        return false
+    } else
 
     if !fermat_little(&n) {
         return false
-    } else {
-        miller_rabin(&n);
+    } else
+
+    if !miller_rabin(&n, &mr_rounds) {
+        return false
     }
 
     true
@@ -59,13 +57,34 @@ fn prime_eh(n: &Int) -> bool {
 
 fn fermat_little(n: &Int) -> bool {
 
-    // let mut rng = thread_rng();
+    let mut rng = thread_rng();
+
     let somea: Int = rng.gen_uint_below(n);
+
+    // let called = Instant::now();
+    
     let res = somea.pow_mod( &(n - Int::one()) , n );
+
+    // let did_the_powmod = called.elapsed();
+
     // println!("Doing a little fermat number here:\nTried with {}\nleft a residue {}", somea, res);
+
+    // println!("Took {:?} to generate a rand brat.\n{:?} to do the pow mod",brought_the_rand,did_the_powmod);
+    // println!("Powmod took: {:?}",did_the_powmod);
+    
     res == Int::one()
 }
 
-fn miller_rabin( _n : &Int ) -> bool {
+fn miller_rabin( _n : &Int , _mr_rounds : &u8) -> bool {
+    true
+}
+
+fn primitive_primality_test (n : &Int) -> bool {
+    for p in SMALL_PRIMES.iter() {
+
+        // println!("testing with {}",p);
+        if n % Int::from(*p) == 0 { return false }
+
+    }
     true
 }
