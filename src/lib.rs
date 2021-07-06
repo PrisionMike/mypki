cd2632d40a5e42f8e86af84fe4f92fcc8419f191
PrisionMike <su.sh2396@gmail.com>
2021-07-06 23:11:05 +0530

    Put all funcs to lib from main

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use std::time::Duration;
+use std::time::{Duration,Instant};
 use std::u8;
 use ramp::Int;
+use ramp::RandomInt;
+use rand::prelude::*;
 
 /*
 #[macro_export]
@@ -13,6 +15,165 @@ macro_rules! time_it {
     }
 }
 */
+
+
+pub fn prime_eh(inquiry: &Query) -> Reply {
+    
+    let n = inquiry.get_int();
+
+    let tm = match inquiry.get_flags() {
+        0 => false,
+        1 => true,
+        _ => true
+    };
+
+    let mr_rounds : u8 = 16;        // MILLER RABBIN ROUNDS
+
+    let mut output = Reply::new(false);
+
+    let t1 = if tm {
+
+        let tic: Instant = Instant::now();
+
+        let t1: bool = primitive_primality_test(n).get_res();
+
+        let toc: Duration = tic.elapsed();
+
+        output.push_that_time(&toc);
+
+        t1
+    } else {
+        primitive_primality_test(n).get_res()
+    };
+
+    if !t1 {
+        // out.set_res(false)
+        return output;
+    }
+
+    let t2 = if tm {
+        let tic2: Instant = Instant::now();
+
+        let t2: bool = fermat_little(n).get_res();
+
+        let toc2: Duration = tic2.elapsed();
+
+        output.push_that_time(&toc2);
+
+        t2
+    } else {
+        fermat_little(n).get_res()
+    };
+
+    if !t2 {
+        return output;
+    }
+
+    let t3 = if tm {
+        let tic3: Instant = Instant::now();
+
+        let t3: bool = miller_rabin( n, &mr_rounds ).get_res();
+
+        let toc3: Duration = tic3.elapsed();
+
+        output.push_that_time(&toc3);
+
+        t3
+    } else {
+        miller_rabin( n, &mr_rounds ).get_res()
+    };
+
+    if !t3 {
+        return output;
+    }
+
+    output.set_res(true);
+
+    output
+}
+
+fn fermat_little(n: &Int) -> Reply {
+
+    let mut output = Reply::new(false);
+
+    let mut rng: ThreadRng = thread_rng();
+
+    let somea: Int = rng.gen_uint_below(n);
+    
+    let res = somea.pow_mod( &(n - Int::one()) , n );
+    
+    output.set_res(res == Int::one());
+
+    output
+}
+
+fn miller_rabin( n : &Int , mr_rounds : &u8) -> Reply {
+    
+    let mut output = Reply::new(false);
+    
+    let one: &Int = &Int::one();
+    let two: &Int = &Int::from(2);
+
+    let mut nminone : Int = n - one;
+
+    let mut s : u16 = 0;
+    while nminone.is_even() {
+
+        s = s + 1;
+
+        nminone = nminone.divmod(two).0;
+
+    }
+
+    let d: Int = nminone;
+
+    let mut rng: ThreadRng = thread_rng();
+
+    for _i in 1..*mr_rounds {
+
+        let base : Int = rng.gen_uint_below( &(n - 4) ) + two;
+
+        let mut x: Int = base.pow_mod(&d,n);
+
+        if x == *one || x == n - one {
+            continue;
+        }
+
+        let mut flag: bool = false;
+        for _j in 1..s-1 {
+            x = x.pow_mod( two, n );
+            if x == n - one {
+                flag = true;
+                break;
+            }
+        }
+
+        if flag {
+            continue;
+        } else {
+            return output
+        }
+
+    }
+
+    output.set_res(true);
+    output
+}
+
+fn primitive_primality_test (n : &Int) -> Reply {
+
+    let mut output : Reply = Reply::new(false);
+    for p in SMALL_PRIMES.iter() {
+
+        // println!("testing with {}",p);
+        if n % Int::from(*p) == 0 { return output }
+
+    }
+    
+    output.set_res(true);
+
+    output
+}
 
 pub struct Query<'a> {
 
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::time::{Duration, Instant};
 
 fn main() {
 
-    let tm : bool = true;
+    let tm : bool = true;       // TELEMETRY SWITCH. 'true : bool' to measure timing.
     
     let mut rng = thread_rng();
     
@@ -48,6 +48,8 @@ fn main() {
                             result.timings[0],
                         result.timings[1],
                     result.timings[2]);
+
+                println!("We forced the {}th bit to be high",topcap);
             
             }
 
@@ -55,162 +57,4 @@ fn main() {
         }
     }
 
-}
-
-fn prime_eh(inquiry: &Query) -> Reply {
-    
-    let n = inquiry.get_int();
-
-    let tm = match inquiry.get_flags() {
-        0 => false,
-        1 => true,
-        _ => true
-    };
-
-    let mr_rounds : u8 = 16;        // MILLER RABBIN ROUNDS
-
-    let mut output = Reply::new(false);
-
-    let t1 = if tm {
-
-        let tic: Instant = Instant::now();
-
-        let t1: bool = primitive_primality_test(n).get_res();
-
-        let toc: Duration = tic.elapsed();
-
-        output.push_that_time(&toc);
-
-        t1
-    } else {
-        primitive_primality_test(n).get_res()
-    };
-
-    if !t1 {
-        // out.set_res(false)
-        return output;
-    }
-
-    let t2 = if tm {
-        let tic2: Instant = Instant::now();
-
-        let t2: bool = fermat_little(n).get_res();
-
-        let toc2: Duration = tic2.elapsed();
-
-        output.push_that_time(&toc2);
-
-        t2
-    } else {
-        fermat_little(n).get_res()
-    };
-
-    if !t2 {
-        return output;
-    }
-
-    let t3 = if tm {
-        let tic3: Instant = Instant::now();
-
-        let t3: bool = miller_rabin( n, &mr_rounds ).get_res();
-
-        let toc3: Duration = tic3.elapsed();
-
-        output.push_that_time(&toc3);
-
-        t3
-    } else {
-        miller_rabin( n, &mr_rounds ).get_res()
-    };
-
-    if !t3 {
-        return output;
-    }
-
-    output.set_res(true);
-
-    output
-}
-
-fn fermat_little(n: &Int) -> Reply {
-
-    let mut output = Reply::new(false);
-
-    let mut rng: ThreadRng = thread_rng();
-
-    let somea: Int = rng.gen_uint_below(n);
-    
-    let res = somea.pow_mod( &(n - Int::one()) , n );
-    
-    output.set_res(res == Int::one());
-
-    output
-}
-
-fn miller_rabin( n : &Int , mr_rounds : &u8) -> Reply {
-    
-    let mut output = Reply::new(false);
-    
-    let one: &Int = &Int::one();
-    let two: &Int = &Int::from(2);
-
-    let mut nminone : Int = n - one;
-
-    let mut s : u16 = 0;
-    while nminone.is_even() {
-
-        s = s + 1;
-
-        nminone = nminone.divmod(two).0;
-
-    }
-
-    let d: Int = nminone;
-
-    let mut rng: ThreadRng = thread_rng();
-
-    for _i in 1..*mr_rounds {
-
-        let base : Int = rng.gen_uint_below( &(n - 4) ) + two;
-
-        let mut x: Int = base.pow_mod(&d,n);
-
-        if x == *one || x == n - one {
-            continue;
-        }
-
-        let mut flag: bool = false;
-        for _j in 1..s-1 {
-            x = x.pow_mod( two, n );
-            if x == n - one {
-                flag = true;
-                break;
-            }
-        }
-
-        if flag {
-            continue;
-        } else {
-            return output
-        }
-
-    }
-
-    output.set_res(true);
-    output
-}
-
-fn primitive_primality_test (n : &Int) -> Reply {
-
-    let mut output : Reply = Reply::new(false);
-    for p in SMALL_PRIMES.iter() {
-
-        // println!("testing with {}",p);
-        if n % Int::from(*p) == 0 { return output }
-
-    }
-    
-    output.set_res(true);
-
-    output
 }
